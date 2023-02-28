60d3577d41cb87c51c24fd2777cb52d5e2825ce8
DCST <dcst@vsu.edu.ph>
2023-02-28 16:20:20 +0800

    Add project files.

---
+++ financialManagementProject/CashBalance.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace financialManagementProject
+{
+    class CashBalance
+    {
+        public static int CASH_BALANCE = 99500;
+
+        public int Amount { get; set; };
+    }
+    public CashBalance(int amount)
+    {
+        this.Amount = amount;
+    }
+}
---
+++ financialManagementProject/PersonalAccount.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace financialManagementProject
+{
+    class PersonalAccount
+    {
+        public static int ACCOUNT_ID_COUNTER = 0;
+        public string AccName { get; set; }
+        public int Amount { get; private set; }
+
+    }
+    public PersonalAccount(string name)
+    {
+        this.Amount = amount;
+    }
+}
