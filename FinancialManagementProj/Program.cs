be49885b2d7e6d67cd6896e32379b620d526011c
Christian Barte <chanixxx0@gmail.com>
2023-03-06 23:50:50 +0800

    ver07

--- FinancialManagementProj/Program.cs
+++ FinancialManagementProj/Program.cs
@@ -2,7 +2,14 @@
 {
     public static void Main(string[] args)
     {
+        //Console.WriteLine("I can't wait for Arthdal Chronicles season 2");
 
+        /*
+         * List<Transaction> transactions = new List<Transaction>();
 
+        Transaction.AddTransaction(transactions, "Salary", 2000, new DateTime(2022, 2, 1));
+        Transaction.AddTransaction(transactions, "Rent", -1000, new DateTime(2022, 2, 15));
+        Transaction.AddTransaction(transactions, "Groceries", -200, new DateTime(2022, 2, 20));
+        */
     }
 }
